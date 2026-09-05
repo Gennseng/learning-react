7511be68dd0c6f6746824564ad7f924381ed0b30
Gennseng <paolo.g.ong03@gmail.com>
2026-09-05 16:36:31 +0800

    Started Data-driven section of course
    Also hello from the git terminal!

---
+++ data-driven/src/App.jsx
@@ -0,0 +1,10 @@
+import Header from '/src/components/Header.jsx'
+
+
+export default function App () {
+    return (
+        <>
+        <Header />
+        </>
+    )
+}
---
+++ data-driven/src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header>
+            <img className="header-img" src="/src/assets/globe.png" />
+            <h1 className="header-title">my travel journal.</h1>
+        </header>
+    )
+}
